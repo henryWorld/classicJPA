428447b259a2bb302dcb42451e1d18fe4423e261
Paulo Giron <paulo.giron@specsavers.com>
2021-09-06 15:48:16 +0000

    feat: SC-410 Make usage of routing Data Source from common
    Add common jar with dynamic DB

--- src/main/java/com/specsavers/socrates/clinical/ClinicalApplication.java
+++ src/main/java/com/specsavers/socrates/clinical/ClinicalApplication.java
@@ -3,7 +3,7 @@ package com.specsavers.socrates.clinical;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.specsavers.socrates.common", "com.specsavers.socrates.clinical"})
 public class ClinicalApplication {
 
 	public static void main(String[] args) {
